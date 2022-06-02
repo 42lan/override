812e899504036350f6b43503fd5f83580844097d
42lan <amalsago@student.42.fr>
2022-06-02 14:42:04 +0200

    level03: add draft of source file

---
+++ level03/source.c
@@ -0,0 +1,38 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void decrypt(int val)
+{
+	char buff[40]
+
+	"Q}|u`sfg~sf{}|a3"
+
+	if ()
+		system("/bin/sh");
+	else
+		puts("\nInvalid Password");
+}
+
+test()
+{
+	char buff[40];
+
+	//if (?? 21)
+	else
+		decrypt(rand())
+}
+
+int main(void)
+{
+	char buff[32];
+
+	srand(time(0));
+	puts("***********************************");
+	puts("*\t\tlevel03\t\t**");
+	puts("***********************************");
+	printf("Password:");
+	scanf("%d", &buff);
+	test(buff, 0x1337d00d);
+	return(0);
+}
