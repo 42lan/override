8257ba79df6a3b00d0a2b1df24c827fdfbe2c03f
42lan <amalsago@student.42.fr>
2022-08-26 20:45:35 +0200

    level06: partly disassemble core of main() function

--- level07/source.c
+++ level07/source.c
@@ -47,16 +47,42 @@ int read_number(void)
 int main(int ac, char **av, char **env)
 {
   buffer[464];
+  int i;
+  char buffer[100] = {0};
 
   esp+0x1c = av[1];
   esp+0x18 = env[1];
   canary = gs:0x14; //esp+0x1cc
-  [24] = {0};
-  [100] = {0};
+
   [...]
-  esp+0x14 = -1;
-  //strlen(av[0]);
-  memset(av[0], 0, 443);
+  memset(av[0], 0, strlen(av[0]));
+
+  i = -1; //esp+0x14
+  while(env[++i])
+    memset(env[i], 0, strlen(env[i]));
+  puts(
+    "----------------------------------------------------\n"
+    "  Welcome to wil's crappy number storage service!   \n"
+    "----------------------------------------------------\n"
+    " Commands:                                          \n"
+    "    store - store a number into the data storage    \n"
+    "    read  - read a number from the data storage     \n"
+    "    quit  - exit the program                        \n"
+    "----------------------------------------------------\n"
+    "   wil has reserved some storage :>                 \n"
+    "----------------------------------------------------\n"
+  )
+  do {
+    printf("Input command: ");
+    fgets(command, 20, stdin);
+    strlen(command);
+    if(memcmp(command, "store", 5) == 0)
+      store_number(); // store_number(&number);
+    else if(memcmp(command, "read", 4) == 0)
+    else if(memcmp(command, "quit", 4) == 0)
+    else
+      printf(" Failed to do %s command\n", command);
+  }
 
   // :( Check if the canary died (if the value was modified). It is the sign to evacuate from the mine!
   if((canary ^ gs:0x14) != 0)
