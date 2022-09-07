3c8c9cb2ac6daa629d19b411b397c24901085625
42lan <amalsago@student.42.fr>
2022-09-07 17:27:54 +0200

    level09: partly disassemble core functions

--- level09/source.c
+++ level09/source.c
@@ -10,6 +10,34 @@ void secret_backdoor(void)
   system(command);
 }
 
+void set_msg(char *buffer)
+{
+}
+
+void set_username(char *buffer)
+{
+  int i = -1;
+  char str[128] = {0};
+  //str[160] 0x7fffffffe400
+
+  puts(">: Enter your username");
+  printf(">>: ");
+  fgets(buffer, 128, stdin);
+  while(++i <= 40 && str[i])
+    buffer[i + 140] = str[i];
+  printf(">: Welcome, %s", buffer + 140);
+}
+
+void handle_msg(void)
+{
+  //buffer[192]
+  char buffer[180];
+  memset(buffer + 140, 0, 40);
+  set_username(buffer);
+  set_msg(buffer);
+  puts(">: Msg sent!");
+}
+
 int main(void)
 {
   puts(
