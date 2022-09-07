95e7c0a59412a24436b39032f03cf539eca710a2
42lan <amalsago@student.42.fr>
2022-09-07 16:12:59 +0200

    level09: disassemble main() function

---
+++ level09/source.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  puts(
+    "--------------------------------------------\n"
+    "|   ~Welcome to l33t-m$n ~    v1337        |\n"
+    "--------------------------------------------\n"
+  );
+  handle_msg();
+  return(0);
+}
